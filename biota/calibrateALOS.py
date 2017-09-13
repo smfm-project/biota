e36a5f959edec0680c797778e62cc77514c57297
sambowers <sam.bowers@ed.ac.uk>
2017-09-13 19:07:49 +0100

    Now calibrates to AGB, and generates summary figures

--- biota/calibrateALOS.py
+++ biota/calibrateALOS.py
@@ -1,14 +1,70 @@
 #!/usr/bin/env python
 
 import argparse
-from osgeo import gdal
+import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+from osgeo import gdal
 
 """
 This is a script to calibrate ALOS mosaic tiles (and, eventually, cross-calibrate ALOS-1/ALOS-2)
 """
 
+
+def generateFilenames(lat, lon, year, data_dir):
+    """
+    Determines ALOS mosaic filename for a given year, latitutude and longitude.
+    """
+
+    # Test that inputs are of reasonable lats/lons/years
+    assert lat < 90. or lat > -90., "Latitude must be between -90 and 90 degrees."
+    assert lon < 180. or lon > -180., "Longitude must be between -180 and 180 degrees."
+    assert (year >= 2007 and year <= 2010) or (year >= 2015 and year <= datetime.datetime.now().year), "Years must be in the range 2007 - 2010 and 2015 - present. Your year was %s."%str(year)
+    
+    # Get hemisphere
+    hem_NS = 'S' if lat < 0 else 'N'
+    hem_EW = 'W' if lon < 0 else 'E'
+    
+    # Get lat/lon for directory (lat/lon of upper-left hand corner of 5x5 tiles)
+    lat_dir = hem_NS + str(abs(lat + (5 - lat) % 5)).zfill(2)
+    lon_dir = hem_EW +  str(abs(lon - 5 + (5 - lon) % 5)).zfill(3)
+    
+    # Get lat/lon for filename
+    lat_file = hem_NS + str(abs(lat)).zfill(2)
+    lon_file = hem_EW + str(abs(lon)).zfill(3)
+
+    # Directories and files have standardised pattern
+    name_pattern = '%s%s_%s_%s'
+    
+    # Directory name patterns name patterns are different for ALOS-1/ALOS-2
+    if year >= 2015:
+        name_pattern += '_F02DAR'
+
+    # Generate directory name
+    directory = data_dir + '/' + name_pattern%(lat_dir, lon_dir, str(year)[-2:], 'MOS') + '/'
+
+    # Generate file names
+    HV_file = directory + name_pattern%(lat_file, lon_file, str(year)[-2:], 'sl_HV')
+    mask_file = directory + name_pattern%(lat_file, lon_file, str(year)[-2:], 'mask')
+    
+    return HV_file, mask_file
+
+
+def openTile(HV_file, mask_file):
+    """
+    Opens ALOS mosaic tile, returns gdal dataset and array.
+    """
+    
+    HV_ds = gdal.Open(HV_file, 0)
+    HV_data = HV_ds.ReadAsArray()
+
+    mask_ds = gdal.Open(mask_file, 0)
+    mask_data = mask_ds.ReadAsArray()
+    
+    data_out = np.ma.array(HV_data, mask = np.logical_or(mask_data != 255, HV_file == 0))
+    
+    return HV_ds, data_out
 
 
 def calibrateToGamma0(DN):
@@ -18,26 +74,123 @@ def calibrateToGamma0(DN):
     Global 25m Resolution PALSAR-2/PALSAR Mosaic and Forest/Non-Forest Map (FNF): Dataset Description
     """
     
-    gamma0 = 10 * np.log10(DN.astype(np.float) ** 2) - 83. # units = decibels
-    gamma0 = 10 ** (gamma0 / 10.) # Natural units
+    gamma0 = 10 * np.ma.log10(DN.astype(np.float) ** 2) - 83. # units = decibels
+    gamma0 = 10 ** (gamma0 / 10.) # Convert to natural units
     
     return gamma0
 
 
-file_2007 = '/home/sbowers3/DATA/ALOS_data/ALOS_mosaic/kilwa/S10E035_07_MOS/S11E038_07_sl_HV'
-file_2016 = '/home/sbowers3/DATA/ALOS_data/ALOS_mosaic/kilwa/S10E035_16_MOS_F02DAR/S11E038_16_sl_HV_F02DAR'
+def calibrateToAGB(gamma0):
+    """
+    Placeholder equation to calibrate backscatter (gamma0) to AGB (tC/ha).
+    """
+    
+    AGB = 715.667 * gamma0 - 5.967
+    
+    return AGB
 
 
-ds_2007 = gdal.Open(file_2007, 0)
-ds_2016 = gdal.Open(file_2016, 0)
+def buildMap(fig, ax, data, lat, lon, title ='', cbartitle = '', vmin = 10., vmax = 60., cmap = 'YlGn'):
+    """
+    Builds a standardised map for overviewFigure().
+    """
+    
+    im = ax.imshow(data, vmin = vmin, vmax = vmax, cmap = cmap, interpolation = 'nearest')
+    
+    ax.set_xticks(np.arange(0,4501,450))
+    ax.set_yticks(np.arange(0,4501,450))
+    ax.set_xticklabels(np.arange(lon,lon + 1.01, 0.1))
+    ax.set_yticklabels(np.arange(lat, lat - 1.01, - 0.1))
+    ax.tick_params(labelsize = 5)
+    ax.set_xlabel('Longitude', fontsize = 5)
+    ax.set_ylabel('Latitude', fontsize = 5)
+    #ax.grid()
+    ax.set_title(title, fontsize = 8)
+    
+    cbar = fig.colorbar(im, ax = ax, fraction = 0.046, pad = 0.04)
+    cbar.ax.tick_params(labelsize = 6)
+    cbar.set_label(cbartitle, fontsize = 7)
+    
 
-data_2007 = ds_2007.ReadAsArray()
-data_2016 = ds_2016.ReadAsArray()
+def overviewFigure(AGB_t1, AGB_t2, t1, t2, lat, lon, output_dir = os.getcwd()):
+    """
+    Create an overview image image showing biomass and proportional biomass change for the tile being processed.
+    """
+    
+    # Update masks to exclude areas outisde forest definition. Good for visualisation
+    AGB_t1 = np.ma.array(AGB_t1, mask = np.logical_or(AGB_t1.mask, AGB_t1 < 10.))
+    AGB_t2 = np.ma.array(AGB_t2, mask = np.logical_or(AGB_t2.mask, AGB_t1 < 10.))
+    
+    
+    AGB_change = (AGB_t2 - AGB_t1) / (t2 - t1) # tC/ha/yr
+
+    AGB_pcChange = 100 * (AGB_change / AGB_t1) # %/yr
+    
+    fig = plt.figure(figsize = (7, 6))
+    
+    # Plot a map of AGB at t1
+    ax1 = fig.add_subplot(2, 2, 1)
+    buildMap(fig, ax1, AGB_t1, lat, lon, title = 'AGB %s'%str(t1), cbartitle = 'tC/ha')
+    
+    # Plot a map of AGB at t2
+    ax2 = fig.add_subplot(2, 2, 2)
+    buildMap(fig, ax2, AGB_t2, lat, lon, title = 'AGB %s'%str(t2), cbartitle = 'tC/ha')    
+    
+    # Plot a map of absolute AGB change   
+    ax3 = fig.add_subplot(2, 2, 3)
+    buildMap(fig, ax3, AGB_change, lat, lon, title = 'AGB change (%s-%s)'%(str(t1),str(t2)), cbartitle = 'tC/ha/yr',
+             vmin = -10., vmax = 10., cmap = 'RdBu')    
+    
+    # Plot a map of % AGB change
+    ax4 = fig.add_subplot(2, 2, 4)
+    buildMap(fig, ax4, AGB_pcChange, lat, lon, title = 'AGB change (%s-%s)'%(str(t1),str(t2)), cbartitle = '%/yr',
+             vmin = -50., vmax = 50., cmap = 'RdBu')    
+    
+    plt.tight_layout()
+    
+    hem_NS = 'S' if lat < 0 else 'N'
+    hem_EW = 'W' if lon < 0 else 'E'
+    
+    filename = '/overview_%s%s.png'%(hem_NS + str(abs(lat)), hem_EW + str(abs(lon)))
+    
+    plt.savefig(output_dir + filename, dpi = 150)
+    plt.close()
 
 
+data_dir = '/home/sbowers3/DATA/ALOS_data/ALOS_mosaic/kilwa/'
+output_dir = '/home/sbowers3/DATA/ALOS_data/ALOS_mosaic/kilwa/'
+
+t1 = 2007
+t2 = 2016
+
+lat = -11
+lon = 38
+
+HVfile_t1, maskfile_t1 = generateFilenames(lat, lon, t1, data_dir)
+HVfile_t2, maskfile_t2 = generateFilenames(lat, lon, t2, data_dir)
+
+ds_t1, data_t1 = openTile(HVfile_t1, maskfile_t1)
+ds_t2, data_t2 = openTile(HVfile_t2, maskfile_t2)
+
+gamma0_t1 = calibrateToGamma0(data_t1)
+gamma0_t2 = calibrateToGamma0(data_t2)
+
+AGB_t1 = calibrateToAGB(gamma0_t1)
+AGB_t2 = calibrateToAGB(gamma0_t2)
+
+overviewFigure(AGB_t1, AGB_t2, t1, t2, lat, lon, output_dir = output_dir)
+
+
+
+
+
+
+
+
+"""
 
 def hist_match(source, template):
-    """
+    '''
     Adjust the pixel values of a grayscale image such that its histogram
     matches that of a target image
 
@@ -52,7 +205,7 @@ def hist_match(source, template):
     -----------
         matched: np.ndarray
             The transformed output image
-    """
+    '''
 
     oldshape = source.shape
     source = source.ravel()
@@ -80,9 +233,9 @@ def hist_match(source, template):
 
 
 def normalise(source, template):
-    """
+    '''
     Matches maximum value in template image.
-    """
+    '''
     
     return ((source.astype(np.float) / np.max(source)) * np.max(template)).astype(source.dtype)
 
@@ -99,9 +252,9 @@ def medianFit(source, template):
 
     
 def linearFit(source, template):
-    """
+    '''
     Matches source to template with linear regression, based on log10 of source and template
-    """
+    '''
     
     import scipy.stats
     
@@ -112,28 +265,5 @@ def linearFit(source, template):
     
     return 10 ** ((slope * np.log10(source)) + intercept)
     
-    
+""" 
 
-#data_2016 = hist_match(data_2016, data_2007)
-#data_2016 = normalise(data_2016, data_2007)
-#data_2016 = medianFit(data_2016, data_2007)
-
-
-data_2007 = calibrateToGamma0(data_2007)
-data_2016 = calibrateToGamma0(data_2016)
-
-#data_2016 = linearFit(data_2016, data_2007)
-
-
-
-plt.subplot(121)
-plt.imshow(data_2007, vmin = 0, vmax = 0.06)
-plt.colorbar()
-plt.subplot(122)
-plt.imshow(data_2016, vmin = 0, vmax = 0.06)
-plt.colorbar()
-plt.show()
-
-
-#plt.scatter(np.log10(data_2007[::10,::10]), np.log10(data_2016[::10,::10]))
-#plt.show()
