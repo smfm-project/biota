93abae15b48f3faa52d3042bed589dbf707f0354
sambowers <sam.bowers@ed.ac.uk>
2017-09-29 13:55:55 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='biota',
+      version='0.1',
+      description='Tools to generate forest change maps from ALOS PALSAR/PALSAR-2 mosaic data.',
+      url='https://bitbucket.org/sambowers/biota',
+      author='Samuel Bowers',
+      author_email='sam.bowers@ed.ac.uk',
+      license='GNU General Pylic License',
+      packages=['biota'],
+      zip_safe=False)
