67246208e7995be9393253c837b6838db87c26ec
sambowers <sam.bowers@ed.ac.uk>
2018-10-23 11:35:08 +0100

    Fixed ALOS-2 single tile download bug for 2015/16

--- biota/download.py
+++ biota/download.py
@@ -56,6 +56,8 @@ def generateURL(lat, lon, year, large_tile = False):
         # Filename patterns are different for ALOS-1/ALOS-2
         if year <= 2010:
             url = 'ftp://ftp.eorc.jaxa.jp/pub/ALOS/ext1/PALSAR_MSC/25m_MSC/%s/%s/%s_%s_MOS.tar.gz'
+        elif year > 2010 and year < 2017:
+            url = 'ftp://ftp.eorc.jaxa.jp/pub/ALOS-2/ext1/PALSAR-2_MSC/25m_MSC/%s/%s/%s_%s_MOS_F02DAR.tar.gz'
         else:
             url = 'ftp://ftp.eorc.jaxa.jp/pub/ALOS-2/ext2/PALSAR-2_MSC/25m_MSC/%s/%s/%s_%s_MOS_F02DAR.tar.gz'
         
