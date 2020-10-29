e42e36b68d477af3100778ef3d2d146b5fdb413e
Sam <sam.bowers@ed.ac.uk>
2020-10-29 16:11:57 +0000

    Updated for customisation of biomass-backscatter model

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import glob
 from setuptools import setup
 
 setup(name='biota',
@@ -5,6 +6,7 @@ setup(name='biota',
       version='0.2',
       description='Tools to generate aboveground biomass forest change maps from ALOS PALSAR/PALSAR-2 mosaic data.',
       url='https://bitbucket.org/sambowers/biota',
+      data_files=[('./cfg/',glob.glob('./cfg/*'))],
       author='Samuel Bowers',
       author_email='sam.bowers@ed.ac.uk',
       license='GNU General Public License',
