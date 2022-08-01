1813a5625464c1bfdd2eba6548f6f169c59420b8
Kristian Bodolai <69843715+KBodolai@users.noreply.github.com>
2022-08-01 15:43:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='biota',
       packages = ['biota'],
-      version='0.2',
+      version='0.3',
       description='Tools to generate aboveground biomass forest change maps from ALOS PALSAR/PALSAR-2 mosaic data.',
       url='https://bitbucket.org/sambowers/biota',
       data_files=[('./cfg/',glob.glob('./cfg/*'))],
