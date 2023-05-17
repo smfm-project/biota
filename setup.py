87662de7244ce7b79d99d4aebb5cc7e9205ec62f
Kristian Bodolai <kristian@space-intelligence.com>
2023-05-17 10:01:47 +0100

    add basic requirements, up version

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup
 
 setup(name='biota',
       packages = ['biota'],
-      version='0.3',
+      version='0.4',
       description='Tools to generate aboveground biomass forest change maps from ALOS PALSAR/PALSAR-2 mosaic data.',
       url='https://github.com/smfm-project/biota',
       data_files=[('./cfg/',glob.glob('./cfg/*'))],
       author='Samuel Bowers',
       author_email='sam.bowers@ed.ac.uk',
       license='GNU General Public License',
-      zip_safe=False
+      zip_safe=False,
       install_requires=[
-          "numpy>=1.21",
+          "numpy",
           "matplotlib",
           "pillow",
           "scipy",
